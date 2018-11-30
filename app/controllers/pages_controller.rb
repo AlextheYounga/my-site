2978ee7a23995d4dbab2915441e08b94c34b701c
alex <alex@izoox.com>
2018-11-29 19:59:15 -0700

    Got Home Page to Show

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'rss'
+require 'stock_quote'
+
+class PagesController < ApplicationController
+
+
+  def home
+    # @articles = Article.order("created_at desc").limit(3)
+
+  end
+
+  def about
+  end
+
+  def readinglist
+  end
+
+  def projects
+  end
+
+  def playground
+    # @user_stocks = User.last.stocks
+
+  end
+
+end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'readinglist', to: 'pages#readinglist'
+  get 'projects', to: 'pages#projects'
+  get 'playground', to: 'pages#playground'
+  get 'search_stocks', to: 'stocks#search'
+
+  resources :articles, except: [:signup]
+
+  get 'signup', to: 'users#signup'
+  resources :users, except: [:signup]
+
+  get 'login', to: 'sessions#login'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
+  resources :posts, only: [:index]
+  resources :user_stocks, only: [:create, :destroy]
+
 end
