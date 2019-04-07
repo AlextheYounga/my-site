edec9e31e54bf8a207f3ae67175251672552fc68
alex <alex@izoox.com>
2019-04-06 23:17:34 -0600

    Rebuilding home

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
     });
   }); 
 
-//Timeline
+Timeline
 $(document).ready(function(){
 
     window.sr = ScrollReveal();
@@ -75,24 +75,24 @@ $(document).ready(function(){
   
   });
   
-// //Carousel
-//   // Instantiate the Bootstrap carousel
-//   $('.multi-item-carousel').carousel({
-//     interval: false
-//   });
+//Carousel
+  // Instantiate the Bootstrap carousel
+  $('.multi-item-carousel').carousel({
+    interval: false
+  });
   
-//   // for every slide in carousel, copy the next slide's item in the slide.
-//   // Do the same for the next, next item.
-//   $('.multi-item-carousel .item').each(function(){
-//     var next = $(this).next();
-//     if (!next.length) {
-//       next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
+  // for every slide in carousel, copy the next slide's item in the slide.
+  // Do the same for the next, next item.
+  $('.multi-item-carousel .item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
     
-//     if (next.next().length>0) {
-//       next.next().children(':first-child').clone().appendTo($(this));
-//     } else {
-//         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-//     }
-//   });
+    if (next.next().length>0) {
+      next.next().children(':first-child').clone().appendTo($(this));
+    } else {
+        $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+    }
+  });
