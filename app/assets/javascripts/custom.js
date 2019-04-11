9aae3f5938741d518ba01e1ecd80fb295a20b544
alex <alex@izoox.com>
2019-04-10 19:41:32 -0600

    Reading list

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,79 +1,10 @@
-//Reading List
-$(function() {
 
-    $('.menu-dropdown button').click(function(){
-      var get_id = this.id;
-      var get_current = $('.covers .' + get_id);
-  
-        $('.cover').not( get_current ).hide(500);
-        get_current.show(500);
-    });
-    
-    
-    $('#showall').click(function() {
-        $('.cover').show(500);
-    });
-}); 
 
 //Stock Result
-$(document).ready(function(){
-    $('#stock-lookup-form').on('ajax:complete', function(event, data, status){
-      $('#results').html(data.responseText);
-    });
-  }); 
+
 
 Timeline
-$(document).ready(function(){
 
-    window.sr = ScrollReveal();
-  
-    if ($(window).width() < 768) {
-  
-        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-        }
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    } else {
-        
-        sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    }
-    
-    sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-  
-  });
   
 //Carousel
   // Instantiate the Bootstrap carousel
