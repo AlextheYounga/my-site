4f856e704cfa195f80778b4939c8dc144d96489f
Alex Younger <alex@izoox.com>
2020-03-22 02:15:36 -0600

    Tuning up

--- db/seeds/projects_table_seeder.rb
+++ db/seeds/projects_table_seeder.rb
@@ -4,8 +4,18 @@ puts "Seeing Projects"
 Project.create([
 
     {
+        title: "Hazlitt Data",
+        description: "Coming Soon",
+        image_address: "hazlitt-data.jpg",
+        image_alt: "alex younger hazlitt data website project",
+        project_link: "#",
+        framework: "PHP Laravel",
+        color: "text-yellow",
+        bg_color: "bg-grey-darkest"
+    },
+    {
         title: "Blue Hawaiian",
-        description: "Myself, along with my boss Matt Scalf, were able to take this site from bare designs to launch in about 4 months. The designs were provided by SpireDigital design. It is one of my greatest acheivements.",
+        description: "",
         image_address: "bluehawaiian.jpg",
         image_alt: "alex younger blue hawaiian helicopters website project",
         project_link: "https://www.bluehawaiian.com/",
@@ -15,7 +25,7 @@ Project.create([
     },
     {
         title: "Sundance Helicopters",
-        description: "I have been almost solely responsible for the maintenance and development of this site over the last year.",
+        description: "",
         image_address: "sundance.jpg",
         image_alt: "alex younger sundance helicopters website project",
         project_link: "https://www.sundancehelicopters.com/",
@@ -25,7 +35,7 @@ Project.create([
     },
     {
         title: "Izoox",
-        description: "I currently work for Izoox full time as a developer. We are currently launching a brand new stellar website. It is currently a work-in-progress. The following is a screenshot from what we have so far.",
+        description: "",
         image_address: "izoox.jpg",
         image_alt: "alex younger blue hawaiian helicopters website project",
         project_link: "https://www.izoox.com/",
@@ -34,44 +44,65 @@ Project.create([
         bg_color: "bg-blue-lightest"
     },
     {
-        title: "Parallel Path",
-        description: "I help maintain the website for Parallel Path and several of their clients, a marketing company based out of Boulder CO",
-        image_address: "pp.jpg",
-        image_alt: "alex younger blue hawaiian helicopters website management",
-        project_link: "https://parallelpath.com/",
-        framework: "PHP Laravel with Wordpress",
-        color: "text-grey-dark",
-        bg_color: "bg-grey-lightest"
-    },
-    {
         title: "The Arkenstone",
-        description: "I occasionally help with the maintenance of The Arkenstone, an incredible mineral auction company",
+        description: "",
         image_address: "irocks.jpg",
         image_alt: "alex younger blue hawaiian helicopters website project and management",
         project_link: "https://www.irocks.com/",
         framework: "PHP Laravel",
         color: "text-purple",
         bg_color: "bg-purple-lightest"
-    },
-    {
-        title: "Healing Natural Oils",
-        description: "I help Healing Natural Oils maintain their Wordpress blog and help with any general website management issues.",
-        image_address: "amoils.jpg",
-        image_alt: "alex younger blue hawaiian helicopters website management",
-        project_link: "https://www.amoils.com/",
-        framework: "Wordpress",
-        color: "text-green",
-        bg_color: "bg-green-lightest"
-    },
+    },    
     {
         title: "Contemplative Outreach",
-        description: "I am currently almost solely responsible for the maintenance of Contemplative Outreach's Drupal website",
+        description: "",
         image_address: "co.jpg",
         image_alt: "alex younger contemplative outreach website management",
         project_link: "https://www.contemplativeoutreach.org/",
         framework: "Drupal",
         color: "text-yellow-darker",
         bg_color: "bg-yellow-lightest"
-    }
+    },
+    {
+        title: "Deep Work Planner",
+        description: "Coming Soon",
+        image_address: "deep-work-planner.jpg",
+        image_alt: "alex younger deep work planner website project and management",
+        project_link: "https://github.com/AlextheYounga/deep-work-planner",
+        framework: "PHP Laravel",
+        color: "text-black",
+        bg_color: "bg-grey-lighter"
+    },
+    {
+        title: "Leading Economic Indicators Scraped from 200 Countries",
+        description: "",
+        image_address: "world-map.jpg",
+        image_alt: "alex younger leading economic indicators from 200 countries project",
+        project_link: "https://keepingstock.net/i-built-a-web-scraper-to-analyze-the-economic-soundness-of-200-countries-heres-what-i-found-d3ec4fa8116b",
+        framework: "PHP Laravel",
+        color: "text-orange-darker",
+        bg_color: "bg-orange-lightest"
+    },
+    # {
+    #     title: "Healing Natural Oils",
+    #     description: "",
+    #     image_address: "amoils.jpg",
+    #     image_alt: "alex younger blue hawaiian helicopters website management",
+    #     project_link: "https://www.amoils.com/",
+    #     framework: "Wordpress",
+    #     color: "text-green",
+    #     bg_color: "bg-green-lightest"
+    # },
+    # {
+    #     title: "Parallel Path",
+    #     description: "",
+    #     image_address: "pp.jpg",
+    #     image_alt: "alex younger blue hawaiian helicopters website management",
+    #     project_link: "https://parallelpath.com/",
+    #     framework: "PHP Laravel with Wordpress",
+    #     color: "text-grey-dark",
+    #     bg_color: "bg-grey-lightest"
+    # },
+    
      
 ])
