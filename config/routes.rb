2a59634a79074e5639f36f3fd7b5930da641ada3
Alex Younger <alex@izoox.com>
2020-03-22 23:30:20 -0600

    Forcing SSL

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  match "*path" => redirect("https://www.alextheyounger.me/%{path}"), :constraints => { :protocol => "http://" }
-  match "*path" => redirect("https://www.alextheyounger.me/%{path}"), :constraints => { :subdomain => "" }
-
   mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
