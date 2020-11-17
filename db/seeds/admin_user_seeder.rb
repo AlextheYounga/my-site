4800dafc20860b9c9797aeabe2db669eabd4e077
Alex Younger <alex@izoox.com>
2020-11-17 11:02:01 -0700

    Fixing user seeder

--- db/seeds/admin_user_seeder.rb
+++ db/seeds/admin_user_seeder.rb
@@ -1,4 +1,5 @@
-@user = User.find_by(username: 'alexyounger')
+@user = User.find_by(email: 'alexyounger07@gmail.com')
+
 if (@user.role.nil? || user.admin_security_key.nil?)
     @user.role = 'master'
     @user.admin_security_key = Rails.application.credentials.admin_security_key
