c8ea027474eec1ebe766dd64411a3efdb1f0cdf1
Alex Younger <alex@izoox.com>
2020-11-17 11:38:12 -0700

    Error checking

--- db/seeds/book_categories_seeder.rb
+++ db/seeds/book_categories_seeder.rb
@@ -6,21 +6,21 @@
 #   BookCategory.destroy_all
 # end
 if (BookCategory.any? == false)
-puts "Seeding Book Categories"
-BookCategory.create([
-  { html_selector: "better-living", name: "Better Living" },
-  { html_selector: "philosophy", name: "Philosophy" },
-  { html_selector: "peopleskill", name: "People Skills" },
-  { html_selector: "econ", name: "Economics" },
-  { html_selector: "history", name: "History" },
-  { html_selector: "finance", name: "Finance" },
-  { html_selector: "tech", name: "Technology" },
-  { html_selector: "science", name: "Science" },
-  { html_selector: "psych", name: "Psychology" },
-  { html_selector: "productivity", name: "Productivity" },
-  { html_selector: "autobio", name: "Autobiography" },
-  { html_selector: "pol", name: "Politics" },
-  { html_selector: "biz", name: "Business" },
-  { html_selector: "misc", name: "Miscellaneous" },
-])
+  puts "Seeding Book Categories"
+  BookCategory.create([
+    { html_selector: "better-living", name: "Better Living" },
+    { html_selector: "philosophy", name: "Philosophy" },
+    { html_selector: "peopleskill", name: "People Skills" },
+    { html_selector: "econ", name: "Economics" },
+    { html_selector: "history", name: "History" },
+    { html_selector: "finance", name: "Finance" },
+    { html_selector: "tech", name: "Technology" },
+    { html_selector: "science", name: "Science" },
+    { html_selector: "psych", name: "Psychology" },
+    { html_selector: "productivity", name: "Productivity" },
+    { html_selector: "autobio", name: "Autobiography" },
+    { html_selector: "pol", name: "Politics" },
+    { html_selector: "biz", name: "Business" },
+    { html_selector: "misc", name: "Miscellaneous" },
+  ])
 end
