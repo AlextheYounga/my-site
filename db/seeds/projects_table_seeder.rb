3554de09686a48a5c170ef78a654284526078a57
Alex Younger <alex@izoox.com>
2020-12-06 19:03:03 -0700

    Project edit working

--- db/seeds/projects_table_seeder.rb
+++ db/seeds/projects_table_seeder.rb
@@ -7,95 +7,143 @@ else
 end
 
 puts "Seeing Projects"
-Project.create([
-  {
-    title: "Blue Hawaiian",
-    description: "Professional: I, along with one other person, put together this masterpiece in about 5 months.",
-    image_address: "bluehawaiian.jpg",
-    image_alt: "alex younger blue hawaiian helicopters website project",
-    project_link: "https://www.bluehawaiian.com/",
-    framework: "PHP Laravel",
+i = 1
 
-  },
-  {
-    title: "Sundance Helicopters",
-    description: "Professional: I have been responsible for the ongoing maintenance and occasional redesigns of this site for nearly two years.",
-    image_address: "sundance.jpg",
-    image_alt: "alex younger sundance helicopters website project",
-    project_link: "https://www.sundancehelicopters.com/",
-    framework: "Ruby on Rails",
+screen = "https://www.bluehawaiian.com/"
+$project = Project.new({
+  title: "Blue Hawaiian",
+  description: "Professional: I, along with one other person, put together this masterpiece in about 5 months.",
+  image_address: "bluehawaiian.jpg",
+  image_alt: "alex younger blue hawaiian helicopters website project",
+  project_link: screen,
+  framework: "PHP Laravel",
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
 
-  },
-  {
-    title: "Izoox",
-    description: "Professional: Site co-creator and ongoing maintenance.",
-    image_address: "izoox.jpg",
-    image_alt: "alex younger blue hawaiian helicopters website project",
-    project_link: "https://www.izoox.com/",
-    framework: "PHP Laravel",
+screen = "https://www.sundancehelicopters.com/"
+$project = Project.new({
+  title: "Sundance Helicopters",
+  description: "Professional: I have been responsible for the ongoing maintenance and occasional redesigns of this site for nearly two years.",
+  image_address: "sundance.jpg",
+  image_alt: screen,
+  project_link: "https://www.sundancehelicopters.com/",
+  framework: "Ruby on Rails",
 
-  },
-  {
-    title: "The Arkenstone",
-    description: "Professional: Ongoing Maintenance",
-    image_address: "irocks.jpg",
-    image_alt: "alex younger blue hawaiian helicopters website project and management",
-    project_link: "https://www.irocks.com/",
-    framework: "PHP Laravel",
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
 
-  },
-  {
-    title: "Contemplative Outreach",
-    description: "Professional: Site co-creator and ongoing maintenance.",
-    image_address: "co.jpg",
-    image_alt: "alex younger contemplative outreach website management",
-    project_link: "https://www.contemplativeoutreach.org/",
-    framework: "Wordpress",
+screen = "https://www.izoox.com/"
+$project = Project.new({
+  title: "Izoox",
+  description: "Professional: Site co-creator and ongoing maintenance.",
+  image_address: "izoox.jpg",
+  image_alt: "alex younger blue hawaiian helicopters website project",
+  project_link: screen,
+  framework: "PHP Laravel",
 
-  },
-  {
-    title: "Hazlitt Data",
-    description: "Product of my company, Hazlitt Tech LLC, a data terminal for financial markets. In production.",
-    image_address: "hazlitt-data.jpg",
-    image_alt: "alex younger hazlitt data website project",
-    project_link: "https://github.com/AlextheYounga/hazlitt-data",
-    framework: "PHP Laravel",
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
 
-  },
-  {
-    title: "Fractal Geometry Calculator",
-    description: "Personal: A Python program for determining Hurst exponents and rescale ranges",
-    image_address: "mandelbrot.jpg",
-    image_alt: "alex younger fractal geometry calculator",
-    project_link: "https://github.com/AlextheYounga/fractalGeometryCalculator",
-    framework: "Python",
+screen = "https://www.irocks.com/"
+$project = Project.new({
+  title: "The Arkenstone",
+  description: "Professional: Ongoing Maintenance",
+  image_address: "irocks.jpg",
+  image_alt: "alex younger blue hawaiian helicopters website project and management",
+  project_link: screen,
+  framework: "PHP Laravel",
 
-  },
-  {
-    title: "Word Frequency Calculator",
-    description: "Personal: A Python program for calculating word frequency in a book, the average syllable count, and graphing word occurrence over time.",
-    image_address: "wordfrequency.jpg",
-    image_alt: "alex younger fractal geometry calculator",
-    project_link: "https://github.com/AlextheYounga/word-frequency-calculator",
-    framework: "Python",
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
 
-  },
-  {
-    title: "Plunge Alert Telegram Bot",
-    description: "Personal: A telegram bot that alerts you via telegram if the market has crashed",
-    image_address: "plungealertbot.jpg",
-    image_alt: "alex younger plunge alert telegram bot",
-    project_link: "https://github.com/AlextheYounga/plungeAlertBot",
-    framework: "Python",
+screen = "https://www.contemplativeoutreach.org/"
+$project = Project.new({
+  title: "Contemplative Outreach",
+  description: "Professional: Site co-creator and ongoing maintenance.",
+  image_address: "co.jpg",
+  image_alt: "alex younger contemplative outreach website management",
+  project_link: screen,
+  framework: "Wordpress",
 
-  },
-  {
-    title: "Deep Work Planner",
-    description: "Personal: A Daily Planner inspired by the book Deep Work",
-    image_address: "deep-work-planner.jpg",
-    image_alt: "alex younger deep work planner website project and management",
-    project_link: "https://github.com/AlextheYounga/deep-work-planner",
-    framework: "Ruby on Rails",
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
 
-  },
-])
+screen = "https://github.com/AlextheYounga/hazlitt-data"
+$project = Project.new({
+  title: "Hazlitt Data",
+  description: "Product of my company, Hazlitt Tech LLC, a data terminal for financial markets. In production.",
+  image_address: "hazlitt-data.jpg",
+  image_alt: "alex younger hazlitt data website project",
+  project_link: screen,
+  framework: "PHP Laravel",
+
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
+
+screen = "https://github.com/AlextheYounga/fractalGeometryCalculator"
+$project = Project.new({
+  title: "Fractal Geometry Calculator",
+  description: "Personal: A Python program for determining Hurst exponents and rescale ranges",
+  image_address: "mandelbrot.jpg",
+  image_alt: "alex younger fractal geometry calculator",
+  project_link: screen,
+  framework: "Python",
+
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
+
+screen = "https://github.com/AlextheYounga/word-frequency-calculator"
+$project = Project.new({
+  title: "Word Frequency Calculator",
+  description: "Personal: A Python program for calculating word frequency in a book, the average syllable count, and graphing word occurrence over time.",
+  image_address: "wordfrequency.jpg",
+  image_alt: "alex younger fractal geometry calculator",
+  project_link: screen,
+  framework: "Python",
+
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
+
+screen = "https://github.com/AlextheYounga/plungeAlertBot"
+$project = Project.new({
+  title: "Plunge Alert Telegram Bot",
+  description: "Personal: A telegram bot that alerts you via telegram if the market has crashed",
+  image_address: "plungealertbot.jpg",
+  image_alt: "alex younger plunge alert telegram bot",
+  project_link: screen,
+  framework: "Python",
+
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
+
+screen = "https://github.com/AlextheYounga/deep-work-planner"
+$project = Project.new({
+  title: "Deep Work Planner",
+  description: "Personal: A Daily Planner inspired by the book Deep Work",
+  image_address: "deep-work-planner.jpg",
+  image_alt: "alex younger deep work planner website project and management",
+  project_link: screen,
+  framework: "Ruby on Rails",
+
+})
+i = i + 1
+@project.save
+@project.seed_screen(screen)
