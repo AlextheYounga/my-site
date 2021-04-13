6a304fe39172566f86e102dac6e0e5939cef56c2
Alex Younger <alex@hazlitt.tech>
2021-04-12 23:50:11 -0600

    Trying to fix language scraper

--- app/models/github_language.rb
+++ app/models/github_language.rb
@@ -1,8 +1,8 @@
 require "json"
 require "resolv-replace"
-require "async"
-require "async/barrier"
-require "async/http/internet"
+# require "async"
+# require "async/barrier"
+# require "async/http/internet"
 require "json"
 require "logger"
 require "colorize"
@@ -40,58 +40,48 @@ class GithubLanguage < ActiveRecord::Base
     username = "AlextheYounga"
     api_url = "https://api.github.com/search/repositories?q=user:#{username}"
 
-    begin
-      puts "#{api_url}".yellow
-      logger = Rails.logger
-      oauth = "token #{Rails.application.credentials.github_personal_token}"
-      repoResponse = Nokogiri::HTML(open(
-        api_url,
-        "User-Agent" => username,
-        "Authorization" => oauth,
-        "proxy" => "http://(ip_address):(port)",
-      ), nil, "UTF-8")
+    puts "#{api_url}".yellow
+    logger = Rails.logger
+    oauth = "token #{Rails.application.credentials.github_personal_token}"
+    repoResponse = Nokogiri::HTML(open(
+      api_url,
+      :http_basic_authentication => [
+        username,
+        oauth
+      ],
+      "User-Agent" => username,
+      "Authorization" => oauth,
+      "proxy" => "http://(ip_address):(port)",
+    ), nil, "UTF-8")
 
-      if (repoResponse.nil? == false || repoResponse.empty? == false)
-        jsonResponse = JSON.parse repoResponse
+    if (repoResponse.nil? == false || repoResponse.empty? == false)
+      jsonResponse = JSON.parse(repoResponse)
 
-        Async do
-          internet = Async::HTTP::Internet.new
-          barrier = Async::Barrier.new
 
-          headers = [
-            ["User-Agent", username],
-            ["proxy", "http://(ip_address):(port)"],
-            ["accept", "application/vnd.github.v3+json"],
-            ["Authorization", "token #{Rails.application.credentials.github_personal_token}"],
-          ]
+      jsonResponse["items"].each do |repo|
+        langUrl = repo["languages_url"] if repo.key?("languages_url")
+        if (langUrl.nil? == false)
 
-          jsonResponse["items"].each do |repo|
-            langUrl = repo["languages_url"] if repo.key?("languages_url")
-            if (langUrl.nil? == false)
-              # Spawn an asynchronous task for each topic:
-              barrier.async do
-                puts "#{langUrl}".green
-                langResponse = internet.get(langUrl, headers)
-                langs = JSON.parse(langResponse.read)
+            langResponse = Nokogiri::HTML(open(
+              langUrl,
+              :http_basic_authentication => [
+                username,
+                oauth
+              ],
+              "User-Agent" => username,
+              "Authorization" => oauth,
+              "proxy" => "http://(ip_address):(port)",
+              "accept" => "application/vnd.github.v3+json",
+            ), nil, "UTF-8")
 
-                if (langs.empty? == false)
-                  languages << langs
-                end
-              end
-            end
+            puts "#{langUrl}".green
+            langs = JSON.parse(langResponse)
+
+          if (langs.empty? == false)
+              languages << langs
           end
-
-          # Ensure we wait for all requests to complete before continuing:
-          barrier.wait
-
-        ensure
-          internet&.close
         end
       end
-    rescue => e
-      puts "#{link} - Error - #{e}".red      
-      logger.error e.message
-      logger.error e.backtrace.join("\n")
     end
 
     langSum = {}
@@ -114,7 +104,8 @@ class GithubLanguage < ActiveRecord::Base
       record = GithubLanguage.find_or_initialize_by(language: l)
       record.value = v
       record.save!
-      puts "#{l} - #{v} saved"
+      puts "#{l} - #{v} saved "
+      puts "\n"
     end
   end
 end
