a28cb5de00f399348cc51c2cd4a08bfd7c02406f
Alex Younger <alex@izoox.com>
2021-04-24 15:03:27 -0600

    Added suppressLanguages function

--- app/models/github_language.rb
+++ app/models/github_language.rb
@@ -1,8 +1,5 @@
 require "json"
 require "resolv-replace"
-# require "async"
-# require "async/barrier"
-# require "async/http/internet"
 require "json"
 require "logger"
 require "colorize"
@@ -26,7 +23,6 @@ class GithubLanguage < ActiveRecord::Base
     end
   end
 
-
   def self.slugifyLanguage(string)
     if (string.include? "+")
       string.gsub("+", "plus")
@@ -34,9 +30,34 @@ class GithubLanguage < ActiveRecord::Base
     return string.parameterize
   end
 
+  def self.suppressLanguages(repoSlug, langs)
+    # Sometimes code from certain packages can highly skew your language stats.
+    # For instance, in Django, some asset packages, like Tailwind, are pushed to the repo, and suddenly your Python Django project
+    # is now treated as 90% CSS.
+    # We can suppress that data here by pointing to a repo name and the language you would like to suppress.
+
+    suppressLang = {
+      "hazlitt-data" => {
+        "lang" => "CSS",
+        "suppressBy" => 0.0000021,
+        # Will suppress by whatever value you pass into suppressBy. 
+        # Let's say you Django project has 4259089 lines of CSS code, but only 9 of those lines are yours.
+        # 9 / 4259089 = 0.0000021
+      },
+    }
+
+    if (suppressLang.has_key?(repoSlug))
+      lang = suppressLang[repoSlug]["lang"]
+      suppressBy = suppressLang[repoSlug]["suppressBy"]
+
+      langs[lang] = (langs[lang] * suppressBy).to_i
+    end
+
+    return langs
+  end
 
   def self.fetchGithub
-    languages = []
+    langSum = {}
     username = "AlextheYounga"
     api_url = "https://api.github.com/search/repositories?q=user:#{username}"
 
@@ -47,7 +68,7 @@ class GithubLanguage < ActiveRecord::Base
       api_url,
       :http_basic_authentication => [
         username,
-        oauth
+        oauth,
       ],
       "User-Agent" => username,
       "Authorization" => oauth,
@@ -57,47 +78,46 @@ class GithubLanguage < ActiveRecord::Base
     if (repoResponse.nil? == false || repoResponse.empty? == false)
       jsonResponse = JSON.parse(repoResponse)
 
-
       jsonResponse["items"].each do |repo|
         langUrl = repo["languages_url"] if repo.key?("languages_url")
+        repoSlug = langUrl.split("https://api.github.com/repos/#{username}/")[1].split("/languages")[0]
+
         if (langUrl.nil? == false)
+          langResponse = Nokogiri::HTML(open(
+            langUrl,
+            :http_basic_authentication => [
+              username,
+              oauth,
+            ],
+            "User-Agent" => username,
+            "Authorization" => oauth,
+            "proxy" => "http://(ip_address):(port)",
+            "accept" => "application/vnd.github.v3+json",
+          ), nil, "UTF-8")
 
-            langResponse = Nokogiri::HTML(open(
-              langUrl,
-              :http_basic_authentication => [
-                username,
-                oauth
-              ],
-              "User-Agent" => username,
-              "Authorization" => oauth,
-              "proxy" => "http://(ip_address):(port)",
-              "accept" => "application/vnd.github.v3+json",
-            ), nil, "UTF-8")
+          puts "#{langUrl}".green
+          langs = JSON.parse(langResponse)
 
-            puts "#{langUrl}".green
-            langs = JSON.parse(langResponse)
+          if (langs.empty? == false)            
+            langs = GithubLanguage.suppressLanguages(repoSlug, langs)
+            puts "#{repoSlug} => #{langs}}".light_blue
 
-          if (langs.empty? == false)
-              languages << langs
+            langs.each do |lang, value|
+              if (langSum.key?(lang) == false)
+                langSum[lang] = value
+                next
+              end
+              langSum[lang] = (langSum[lang] + value)
+            end
           end
+          sleep(1)
         end
-      end
-    end
-
-    langSum = {}
-    languages.each do |language|
-      language.each do |lang, value|
-        if (langSum.key?(lang) == false)
-          langSum[lang] = value
-          next
-        end
-        langSum[lang] = (langSum[lang] + value)
       end
     end
 
     if (langSum.blank?)
-      logger.info 'Failed to get language stats; stats are nil.'
-      return "Failed to get language stats".red      
+      logger.info "Failed to get language stats; stats are nil."
+      return "Failed to get language stats".red
     end
 
     langSum.each do |l, v|
@@ -105,7 +125,6 @@ class GithubLanguage < ActiveRecord::Base
       record.value = v
       record.save!
       puts "#{l} - #{v} saved "
-      puts "\n"
     end
   end
 end
